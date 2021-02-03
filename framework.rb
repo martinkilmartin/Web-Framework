87894d16438b02ef6aa475b22b5cdca46f53fac2
sionnach <sionnach.solutions@gmail.com>
2021-02-03 13:09:33 -0500

    Basic Request Routing

---
+++ framework.rb
@@ -0,0 +1,54 @@
+class App
+  def initialize(&block)
+    @routes = RouteTable.new(block)
+  end
+
+  def call(env)
+    request = Rack::Request.new(env)
+    @routes.each do |route|
+      content = route.match(request)
+      return [200, {}, [content]] if content
+    end
+    [404, {}, ["Not found"]]
+  end
+
+  class RouteTable
+    def initialize(block)
+      @routes = []
+      instance_eval(&block)
+    end
+
+    def get(route_spec, &block)
+      @routes << Route.new(route_spec, block)
+      p @routes
+    end
+
+    def each(&block)
+      @routes.each(&block)
+    end
+  end
+  
+  class Route < Struct.new(:route_spec, :block)
+    def match(request)
+      path_components = request.path.split('/')
+      spec_components = route_spec.split('/')
+
+      params = {}
+
+      return nil unless path_components.length == spec_components.length
+
+      path_components.zip(spec_components).each do |path_comp, spec_comp|
+        is_var = spec_comp.start_with?(':')
+        if is_var
+          key = spec_comp.sub(/\A:/, '')
+          params[key] = path_comp
+        else
+          return nil unless path_comp == spec_comp
+        end
+      end
+
+      block.call(params)
+    end
+  end
+end
+
---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative 'framework'
+
+APP = App.new do
+  get '/' do
+    "This is the root"
+  end
+
+  get '/users/:username' do |params|
+    "This is #{params.fetch('username')}!"
+  end
+end
+
